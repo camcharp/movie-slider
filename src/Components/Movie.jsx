aec1b6a8a420aaf8685fb2f467ef0d134defa8f9
Camille Charpentier <cam.charpentier@live.fr>
2019-10-22 15:24:45 +0200

    cleaned app

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -3,11 +3,9 @@ import React from 'react';
 export default function Movie(props) {
 	return (
 		<div className="movie">
-			<div>
-				<h1>{props.movie_data.original_title}</h1>
-				<img src={`https://image.tmdb.org/t/p/w500${props.movie_data.poster_path}`} alt="movie poster" />
-				<p className="rate">{props.movie_data.vote_average}</p>
-			</div>
+			<h1 id="title">{props.movie_data.original_title}</h1>
+			<img src={`https://image.tmdb.org/t/p/w500${props.movie_data.poster_path}`} alt="movie poster" />
+			<p id="rate">{props.movie_data.vote_average}</p>
 		</div>
 	);
 }
